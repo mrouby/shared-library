ec6ad0c5c671bf8d4acc30bfe6268deb49bca63c
Mohammed Rouby <mohammedrouby2000@gmail.com>
2025-11-19 00:44:13 +0200

    Enhance dockerPipeline with build and push stages
    Added stages for building, pushing, deleting local Docker images, and updating manifests in the pipeline.

--- vars/dockerPipeline.groovy
+++ vars/dockerPipeline.groovy
@@ -1,3 +1,48 @@
+def call(Map config = [:]) {
+
+    pipeline {
+        agent any
+
+        stages {
+
+            stage('Build Docker Image') {
+                steps {
+                    script {
+                        buildImage(config.image)
+                    }
+                }
+            }
+
+            stage('Push Docker Image') {
+                steps {
+                    script {
+                        pushImage(config.image)
+                    }
+                }
+            }
+
+            stage('Delete Local Image') {
+                steps {
+                    script {
+                        deleteLocalImage(config.image)
+                    }
+                }
+            }
+
+            stage('Update Manifests') {
+                steps {
+                    script {
+                        updateManifests()
+                    }
+                }
+            }
+
+        }
+    }
+}
+
+// ------------------ FUNCTIONS -------------------
+
 def buildImage(imageName) {
     echo "Building Docker image: ${imageName}"
     sh "docker build -t ${imageName} ."
@@ -20,5 +65,4 @@ def updateManifests() {
 
 def pushManifests() {
     echo "Pushing manifests to repository..."
-    // إذا عندك خطوات push، هنا تتحط
 }
